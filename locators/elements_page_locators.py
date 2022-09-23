4614e78db9303948ee4a0500780cc7f72dd54a7d
svetlanaalt <shunko.svetlana@gmail.com>
2022-09-23 17:28:06 +0300

    fixed go_to_element in BasePage update elements_test.py added test checkboxes

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 
 class TextBoxPageLocators:
-
     # form fields
 
     FULL_NAME = (By.CSS_SELECTOR, 'input[id="userName"]')
@@ -14,3 +13,8 @@ class TextBoxPageLocators:
 
     CREATED_OUTPUT_FORM = (By.CSS_SELECTOR, '#output, #name')
 
+class CheckBoxPageLocators:
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, "button[title='Expand all']")
+    ITEM_LIST = (By.CSS_SELECTOR, "span[class='rct-title']")
+    CHECKED_ITEMS = (By.CSS_SELECTOR, "svg[class='rct-icon rct-icon-check']")
+    OUTPUT_RESULT = (By.CSS_SELECTOR, "span[class='text-success']")
